fb3f56d077d12aba6cc92ad4165e07289f99c230
ajfahim <ajfahim52@gmail.com>
2023-11-23 01:20:40 +0600

    minor changes

--- src/modules/users/users.model.ts
+++ src/modules/users/users.model.ts
@@ -1,5 +1,5 @@
 import { Schema, model } from 'mongoose';
-import { TAddress, TFullName, TOrders, TUser } from './users.interface';
+import { TAddress, TFullName, TOrder, TUser } from './users.interface';
 
 const fullNameSchema = new Schema<TFullName>({
   firstName: { type: String, required: true },
@@ -12,23 +12,23 @@ const addressSchema = new Schema<TAddress>({
   country: { type: String, required: true },
 });
 
-const ordersSchema = new Schema<TOrders>({
+const ordersSchema = new Schema<TOrder>({
   productName: { type: String, required: true },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
 });
 
-const userSchema = new Schema({
-  userId: { type: Number, required: true },
-  username: { type: String, required: true },
+const userSchema = new Schema<TUser>({
+  userId: { type: Number, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   fullName: { type: fullNameSchema, required: true },
   age: { type: Number, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   isActive: { type: Boolean, required: true },
-  hobbies: { type: [String], required: true },
+  hobbies: [{ type: String, required: true }],
   address: { type: addressSchema, required: true },
-  orders: { type: ordersSchema, required: true },
+  orders: [{ type: ordersSchema, required: true }],
 });
 
-export const Users = model<TUser>('User', userSchema);
+export const UsersModel = model<TUser>('User', userSchema);
